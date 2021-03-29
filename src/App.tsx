d02c549c0c841ef6a58880c129056b1d59bb17e5
Ryosuke <ryosuke.san.hana@gmail.com>
2021-03-29 00:45:56 -0700

    :tada: aframe installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//@ts-ignore
+import { Entity, Scene } from "aframe-react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Scene>
+      <a-assets>
+        <img
+          id="groundTexture"
+          src="https://cdn.aframe.io/a-painter/images/floor.jpg"
+          alt=""
+        />
+        <img
+          id="skyTexture"
+          src="https://cdn.aframe.io/a-painter/images/sky.jpg"
+          alt=""
+        />
+      </a-assets>
+
+      <Entity
+        primitive="a-plane"
+        src="#groundTexture"
+        rotation="-90 0 0"
+        height="100"
+        width="100"
+      />
+      <Entity primitive="a-light" type="ambient" color="#445451" />
+      <Entity primitive="a-light" type="point" intensity="2" position="2 4 4" />
+      <Entity
+        primitive="a-sky"
+        height="2048"
+        radius="30"
+        src="#skyTexture"
+        theta-length="90"
+        width="2048"
+      />
+      <Entity
+        geometry={{ primitive: "box" }}
+        material={{ color: "red" }}
+        position={{ x: 0, y: 0, z: -5 }}
+      />
+      <Entity light={{ type: "point" }} />
+      <Entity text={{ value: "Hello, WebVR!" }} />
+
+      <Entity primitive="a-camera">
+        <Entity
+          primitive="a-cursor"
+          animation__click={{
+            property: "scale",
+            startEvents: "click",
+            from: "0.1 0.1 0.1",
+            to: "1 1 1",
+            dur: 150,
+          }}
+        />
+      </Entity>
+    </Scene>
   );
 }
 
